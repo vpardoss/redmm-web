6af3266361dc3bfcf3f03a724536672e499b80f0
vpardoss <vicente.pardo@estudiante.uc.cl>
2025-11-29 11:40:05 -0300

    Update API URL for bus stop requests

--- src/app/api/bus-status/route.ts
+++ src/app/api/bus-status/route.ts
@@ -32,7 +32,7 @@ export async function GET(request: Request) {
   }
 
   const stopIdArray = stopIds.split(',');
-  const API_URL = 'https://spb82m8sd209.share.zrok.io/bus-stop/';
+  const API_URL = 'https://2a5tni6z38fj.share.zrok.io/bus-stop/';
 
   try {
     const responses = await Promise.all(
