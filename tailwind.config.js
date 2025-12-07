fc63cf4ee8965e1f6b5ad605ae6abd159056d487
Vicente Pardo Silva <vicente.pardo@estudiante.uc.cl>
2025-12-07 00:15:13 -0300

    updated to next.js v16.0.7 addressing vulnerability (FIX)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // <-- Crítico
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
 // tailwind.config.ts
 import type { Config } from 'tailwindcss'
 
